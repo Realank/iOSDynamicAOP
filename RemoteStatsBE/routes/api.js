4482e1979614b9701a6990f81e1e34a3aad51fef
Liu Yanbo <yanbo.liu@ucarinc.com>
2018-11-19 11:29:36 +0800

    fix

--- RemoteStatsBE/routes/api.js
+++ RemoteStatsBE/routes/api.js
@@ -10,8 +10,9 @@ router.get('/list', function (req, res, next) {
     let mappingList = result.map((mapping) => {
       if (mapping.filterList) {
         let filterList = mapping.filterList.map((filter) => {
-          filter = { ...filter, operation: 'equal' }
-          console.log('change fileter ' + JSON.stringify(filter))
+          console.log('before filter ' + JSON.stringify(filter))
+          filter['operation'] = 'equal'
+          console.log('change filter ' + JSON.stringify(filter))
           return filter
         })
         mapping.filterList = filterList
